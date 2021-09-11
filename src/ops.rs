e4027395acbf6e34a78515860c52145c703e2d5f
Spruce Weber-Milne <timberdig@gmail.com>
2021-09-11 09:14:46 -0700

    impl Operation::do_op

--- src/ops.rs
+++ src/ops.rs
@@ -8,18 +8,16 @@ pub enum Operation {
   AddAssignSelfU8OtherU8,
 }
 
-// pub fn DoOp(op: Operation, operands: &mut ArrayVec<Value, 3>) {
-//   match op {
-//     Operation::AddSelfU8OtherU8OutU8 => {
-//       let out = &mut operands[2].as_u8_mut();
-//       let slf = &(operands[0].as_u8());
-//       let other = &(operands[1].as_u8());
-
-//       **out = **slf + **other
-//     }
-//     Operation::AddAssignSelfU8OtherU8 => {}
-//   }
-// }
+impl Operation {
+  pub fn do_op(self, operand0: &mut Value, operand1: &mut Value, operand2: Option<&mut Value>) {
+    match self {
+      Operation::AddSelfU8OtherU8OutU8 => {
+        *operand2.unwrap().as_u8_mut() = *operand0.as_u8() + *operand1.as_u8()
+      }
+      Operation::AddAssignSelfU8OtherU8 => *operand0.as_u8_mut() += *operand1.as_u8(),
+    }
+  }
+}
 
 type ValueX3 = ArrayVec<Value, 3>;
 
@@ -40,6 +38,25 @@ mod tests {
   use super::*;
 
   #[test]
+  fn test_do_op() {
+    let mut operand0 = Value {
+      bytes: [1, 0, 0, 0, 0, 0, 0, 0],
+    };
+    let mut operand1 = Value {
+      bytes: [2, 0, 0, 0, 0, 0, 0, 0],
+    };
+    let mut operand2 = Value {
+      bytes: [3, 0, 0, 0, 0, 0, 0, 0],
+    };
+
+    Operation::AddAssignSelfU8OtherU8.do_op(&mut operand0, &mut operand1, None);
+    assert_eq!(operand0.bytes, [3, 0, 0, 0, 0, 0, 0, 0]);
+
+    Operation::AddSelfU8OtherU8OutU8.do_op(&mut operand0, &mut operand1, Some(&mut operand2));
+    assert_eq!(operand2.bytes, [5, 0, 0, 0, 0, 0, 0, 0]);
+  }
+
+  #[test]
   fn test_split_value_mut() {
     let mut operands = ValueX3::new();
     operands.push(Value {
@@ -51,18 +68,9 @@ mod tests {
     operands.push(Value {
       bytes: [3, 0, 0, 0, 0, 0, 0, 0],
     });
+    let (op1, op2, op3) = split_value_mut(&mut operands);
     {
-      let (op1, op2, op3) = split_value_mut(&mut operands);
-      {
-        let op1u8 = op1.as_u8();
-        let op2u8 = op2.as_u8();
-        let op3u8 = op3.as_u8_mut();
-
-        println!("{:?}, {:?}, {:?}", *op1u8, *op2u8, *op3u8);
-
-        *op3u8 = *op1u8 + *op2u8 * 10;
-        assert_eq!(*op3u8, 21);
-      }
+      *op3.as_u8_mut() = *op1.as_u8() + *op2.as_u8() * 10;
     }
 
     assert_eq!(operands[2].bytes, [21, 0, 0, 0, 0, 0, 0, 0]);
